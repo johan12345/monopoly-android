e1997a2088b6bf358f0e1c2d1c1ed431b74c8c93
johan12345 <johan.forstner@gmail.com>
2014-09-19 14:27:39 +0200

    Mehr Kommentare

--- Monopoly/src/de/unikiel/programmierpraktikum/monopoly/utilities/Utilities.java
+++ Monopoly/src/de/unikiel/programmierpraktikum/monopoly/utilities/Utilities.java
@@ -16,34 +16,82 @@ import android.util.DisplayMetrics;
 import android.view.View;
 
 /**
- * @author johan_000
- * Enthält nützliche static-Methoden
+ * Enthält nützliche static-Methoden, die für das Spiel nötig sind
+ * 
+ * @author Miriam Scharnke, Johan v. Forstner
+ * 
  */
 public class Utilities {
+	/**
+	 * Calculate a random integer
+	 * 
+	 * @param min
+	 *            minimum value
+	 * @param max
+	 *            maximum value
+	 * @return a random integer between the min and max values
+	 */
 	public static int randomInt(int min, int max) {
-		return min + (int)(Math.random() * ((max - min) + 1));
+		return min + (int) (Math.random() * ((max - min) + 1));
 	}
 
+	/**
+	 * @return A random Integer between 2 and 12, analogous to throwing two dice
+	 */
 	public static int dice() {
-		return randomInt(1,6) + randomInt(1,6);
-	}
-	
-	public static boolean diceDouble() {
-		return randomInt(1,6) == randomInt(1,6);
+		return randomInt(1, 6) + randomInt(1, 6);
 	}
 
+	/**
+	 * Throw two dice and check if they are the same value
+	 * 
+	 * @return a boolean corresponding to the result
+	 */
+	public static boolean diceDouble() {
+		return randomInt(1, 6) == randomInt(1, 6);
+	}
+
+	/**
+	 * Convert Android's density independent pixels to pixels
+	 * 
+	 * @param dp
+	 *            the amount of dps to convert
+	 * @param context
+	 *            a context
+	 * @return the conversion to pixels
+	 */
 	public static int dpToPx(int dp, Context context) {
-		DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-		int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));       
+		DisplayMetrics displayMetrics = context.getResources()
+				.getDisplayMetrics();
+		int px = Math.round(dp
+				* (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
 		return px;
 	}
 
+	/**
+	 * Convert pixels to Android's density independent pixels
+	 * 
+	 * @param px
+	 *            the amount of pixels to convert
+	 * @param context
+	 *            a context
+	 * @return the conversion to dps
+	 */
 	public static int pxToDp(int px, Context context) {
-		DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-		int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+		DisplayMetrics displayMetrics = context.getResources()
+				.getDisplayMetrics();
+		int dp = Math.round(px
+				/ (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
 		return dp;
 	}
-	
+
+	/**
+	 * Draw a {@link View} and save the output to a bitmap
+	 * 
+	 * @param v
+	 *            the View to use
+	 * @return the bitmap
+	 */
 	public static Bitmap loadBitmapFromView(View v) {
 		Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(),
 				Bitmap.Config.ARGB_8888);
@@ -53,56 +101,121 @@ public class Utilities {
 		return b;
 	}
 
+	/**
+	 * Get the colored circle Drawable ID corresponding to a player index
+	 * 
+	 * @param index
+	 *            the player index
+	 * @return Drawable ID, or -1 if unknown
+	 */
 	public static int getCircleDrawable(int index) {
-		switch(index) {
-		case 0: return R.drawable.ic_circle_1;
-		case 1: return R.drawable.ic_circle_2;
-		case 2: return R.drawable.ic_circle_3;
-		case 3: return R.drawable.ic_circle_4;
-		case 4: return R.drawable.ic_circle_5;
-		case 5: return R.drawable.ic_circle_6;
-		case 6: return R.drawable.ic_circle_7;
-		case 7: return R.drawable.ic_circle_8;
-		case 8: return R.drawable.ic_circle_9;
-		case 9: return R.drawable.ic_circle_10;
-		default: return -1;
+		switch (index) {
+		case 0:
+			return R.drawable.ic_circle_1;
+		case 1:
+			return R.drawable.ic_circle_2;
+		case 2:
+			return R.drawable.ic_circle_3;
+		case 3:
+			return R.drawable.ic_circle_4;
+		case 4:
+			return R.drawable.ic_circle_5;
+		case 5:
+			return R.drawable.ic_circle_6;
+		case 6:
+			return R.drawable.ic_circle_7;
+		case 7:
+			return R.drawable.ic_circle_8;
+		case 8:
+			return R.drawable.ic_circle_9;
+		case 9:
+			return R.drawable.ic_circle_10;
+		default:
+			return -1;
 		}
 	}
 
+	/**
+	 * Get the Drawable ID corresponding to a specific {@link Peg}
+	 * 
+	 * @param peg
+	 *            the Peg
+	 * @return the corresponding Drawable ID, or -1 if unknown
+	 */
 	public static int getPegDrawable(Peg peg) {
-		switch(peg) {
-		case ALBERT_EINSTEIN: return R.drawable.ic_albert_einstein;
-		case EMMY_NOETHER: return R.drawable.ic_emmy_noether;
-		case ERWIN_SCHROEDINGER: return R.drawable.ic_erwin_schroedinger;
-		case MARIE_CURIE: return R.drawable.ic_marie_curie;
-		case MAX_PLANCK: return R.drawable.ic_max_planck;
-		case MICHAEL_FARADAY: return R.drawable.ic_michael_faraday;
-		case RICHARD_FEYNMAN: return R.drawable.ic_richard_feynman;
-		case STEPHEN_HAWKING: return R.drawable.ic_stephen_hawking;
-		case WERNER_HEISENBERG: return R.drawable.ic_werner_heisenberg;
-		case WOLFGANG_PAULI: return R.drawable.ic_wolfgang_pauli;
-		default: return -1;
+		switch (peg) {
+		case ALBERT_EINSTEIN:
+			return R.drawable.ic_albert_einstein;
+		case EMMY_NOETHER:
+			return R.drawable.ic_emmy_noether;
+		case ERWIN_SCHROEDINGER:
+			return R.drawable.ic_erwin_schroedinger;
+		case MARIE_CURIE:
+			return R.drawable.ic_marie_curie;
+		case MAX_PLANCK:
+			return R.drawable.ic_max_planck;
+		case MICHAEL_FARADAY:
+			return R.drawable.ic_michael_faraday;
+		case RICHARD_FEYNMAN:
+			return R.drawable.ic_richard_feynman;
+		case STEPHEN_HAWKING:
+			return R.drawable.ic_stephen_hawking;
+		case WERNER_HEISENBERG:
+			return R.drawable.ic_werner_heisenberg;
+		case WOLFGANG_PAULI:
+			return R.drawable.ic_wolfgang_pauli;
+		default:
+			return -1;
 		}
 	}
 
+	/**
+	 * Get the Color ID corresponding to a field {@link Category}
+	 * 
+	 * @param category
+	 *            the category
+	 * @return the color ID, or -1 if unknown
+	 */
 	public static int getCategoryColor(Category category) {
 		switch (category) {
-		case BROWN: return R.color.category_brown;			
-		case LIGHT_BLUE: return R.color.category_light_blue;			
-		case PINK: return R.color.category_pink;			
-		case ORANGE: return R.color.category_orange;		
-		case RED: return R.color.category_red;			
-		case YELLOW: return R.color.category_yellow;
-		case GREEN: return R.color.category_green;		
-		case DARK_BLUE: return R.color.category_dark_blue;		
-		default: return -1;
+		case BROWN:
+			return R.color.category_brown;
+		case LIGHT_BLUE:
+			return R.color.category_light_blue;
+		case PINK:
+			return R.color.category_pink;
+		case ORANGE:
+			return R.color.category_orange;
+		case RED:
+			return R.color.category_red;
+		case YELLOW:
+			return R.color.category_yellow;
+		case GREEN:
+			return R.color.category_green;
+		case DARK_BLUE:
+			return R.color.category_dark_blue;
+		default:
+			return -1;
 		}
 	}
-	
+
+	/**
+	 * @return the {@link DecimalFormat} used for formatting currencies in this
+	 *         game
+	 */
 	public static DecimalFormat moneyFormat() {
 		return new DecimalFormat("#,##0 eV");
 	}
-	
+
+	/**
+	 * Shortcut method for reading an input stream into a String
+	 * 
+	 * @param input
+	 *            an InputStream
+	 * @return the String read from the stream
+	 * @throws IOException
+	 *             when there is an error reading the stream
+	 */
 	public static String readStream(InputStream input) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(input));
 		try {
@@ -119,7 +232,15 @@ public class Utilities {
 			br.close();
 		}
 	}
-	
+
+	/**
+	 * Shortcut method for Android's visibility attributes
+	 * 
+	 * @param visible
+	 *            visible or not
+	 * @return {@link View.VISIBLE} if visible == true, otherwise
+	 *         {@link View.INVISIBLE}
+	 */
 	public static int visibility(boolean visible) {
 		return visible ? View.VISIBLE : View.INVISIBLE;
 	}
